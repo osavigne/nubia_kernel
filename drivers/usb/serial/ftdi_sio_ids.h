13efd92d192a8588a723e691c494f4409f3984d0
Chenxi Mao <chenxi.mao2013@gmail.com>
2013-10-27 12:21:01 +0800

    Linux 3.4.10

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -786,6 +786,21 @@
 #define RTSYSTEMS_CT29B_PID		0x9e54	/* CT29B Radio Cable */
 #define RTSYSTEMS_RTS01_PID		0x9e57	/* USB-RTS01 Radio Cable */
 
+
+/*
+ * Physik Instrumente
+ * http://www.physikinstrumente.com/en/products/
+ */
+#define PI_VID              0x1a72  /* Vendor ID */
+#define PI_E861_PID         0x1008  /* E-861 piezo controller USB connection */
+
+/*
+ * Kondo Kagaku Co.Ltd.
+ * http://www.kondo-robot.com/EN
+ */
+#define KONDO_VID 		0x165c
+#define KONDO_USB_SERIAL_PID	0x0002
+
 /*
  * Bayer Ascensia Contour blood glucose meter USB-converter cable.
  * http://winglucofacts.com/cables/
